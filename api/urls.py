924576bb818ebee3991fd500d107806bff5560a5
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2021-11-21 04:09:12 +0100

    Done?

--- api/urls.py
+++ api/urls.py
@@ -1,34 +1,25 @@
+from api import views
 from django.urls import path
-from api.views import (
-    CompleteTodoItemView,
-    CompletedTodoitemsOnListView,
-    TodoItemDetailView,
-    TodoitemsListView,
-    TodoitemsOnListView,
-    api_root,
-    TodolistListCreateView,
-    TodolistDetailView,
-)
 
 urlpatterns = [
-    path("", api_root),
-    path("lists/", TodolistListCreateView.as_view(), name="todolist-list"),
-    path("lists/<int:pk>/", TodolistDetailView.as_view(), name="todolist-detail"),
+    path("", views.api_root),
+    path("lists/", views.TodolistListCreateView.as_view(), name="todolist-list"),
+    path("lists/<int:pk>/", views.TodolistDetailView.as_view(), name="todolist-detail"),
     path(
         "lists/<int:pk>/items/",
-        TodoitemsOnListView.as_view(),
+        views.TodoitemsOnListView.as_view(),
         name="todolist-items",
     ),
     path(
         "lists/<int:pk>/items/completed/",
-        CompletedTodoitemsOnListView.as_view(),
+        views.CompletedTodoitemsOnListView.as_view(),
         name="todolist-completed-items",
     ),
-    path("items/", TodoitemsListView.as_view(), name="todoitem-list"),
-    path("items/<int:pk>/", TodoItemDetailView.as_view(), name="todoitem-detail"),
+    path("items/", views.TodoitemsListView.as_view(), name="todoitem-list"),
+    path("items/<int:pk>/", views.TodoItemDetailView.as_view(), name="todoitem-detail"),
     path(
         "items/<int:pk>/complete",
-        CompleteTodoItemView.as_view(),
+        views.CompleteTodoItemView.as_view(),
         name="todoitem-complete",
     ),
 ]
